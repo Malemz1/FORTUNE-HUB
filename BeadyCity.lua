0817759e02c919230ef86b08e6927b3da75ca0cd
Malemz1 <jaezabang@gmail.com>
2024-12-15 17:26:38 +0700

    Update BeadyCity.lua

--- BeadyCity.lua
+++ BeadyCity.lua
@@ -3,7 +3,7 @@
 local keys = {
     ["FORTUNE-D6X-K61-7BA"] = { hwid = "311F338E-A4B6-4AE8-AE25-C56B209C7E1A", used = false },
     ["FORTUNE-PDV-J33-3L6"] = { hwid = "B42CE9D5-5A28-45F1-BAF8-EFBFA6F6B6E1", used = false },
-    ["FORTUNE-MEV-RJM-74N"] = { hwid = nil, used = false },
+    ["FORTUNE-MEV-RJM-74N"] = { hwid = nil, used = true },
     -- เพิ่มคีย์อื่นๆ ตามต้องการ
 }
 
