c396a131e0e0c398239d4c5f615f2671c5a64601
Malemz1 <jaezabang@gmail.com>
2024-12-18 01:54:57 +0700

    Update BeadyCitySecret.lua

--- BeadyCitySecret.lua
+++ BeadyCitySecret.lua
@@ -5,7 +5,7 @@ local keys = {
     ["FORTUNE-PDV-J33-3L6"] = { clientId = "B42CE9D5-5A28-45F1-BAF8-EFBFA6F6B6E1", used = false, admin = false },
     ["FORTUNE-MEV-RJM-74N"] = { clientId = nil, used = false, admin = false },
     ["FORTUNE-ADMIN-ADMIN-ADMIN"] = { clientIds = {"311F338E-A4B6-4AE8-AE25-C56B209C7E1A", "B42CE9D5-5A28-45F1-BAF8-EFBFA6F6B6E1"}, used = false, admin = true },  -- เพิ่ม 2 Client IDs สำหรับแอดมิน
-    -- เพิ่มคีย์อื่นๆ ตามต้องการ
+    -- เพิ่มคีย์อื่นๆ
 }
 
 -- ฟังก์ชันดึง Client ID
