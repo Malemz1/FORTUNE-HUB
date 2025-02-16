e8948bdbea5e6ba843d103d70fc06f4b68cc256c
TheG450 <brfxxx450@gmail.com>
2025-02-16 17:33:31 +0700

    Update BackEnd

--- FeariseBackend.lua
+++ FeariseBackend.lua
@@ -3,7 +3,25 @@
 local player = game.Players.LocalPlayer
 local host = "http://ec2-3-106-212-13.ap-southeast-2.compute.amazonaws.com:3000/api/"
 
-local url = host.."/getaccess?userKey=" .. getgenv().Key
+local url = host.."/getaccess?userKey=" .. getgenv().key
+
+local function FeariseLodded()
+    print([[
+                    
+                 ______              _          _    _       _     
+                |  ____|            (_)        | |  | |     | |    
+                | |__ ___  __ _ _ __ _ ___  ___| |__| |_   _| |__  
+                |  __/ _ \/ _` | '__| / __|/ _ \  __  | | | | '_ \ 
+                | | |  __/ (_| | |  | \__ \  __/ |  | | |_| | |_) |
+                |_|  \___|\__,_|_|  |_|___/\___|_|  |_|\__,_|_.__/ 
+                    | |             | |   | |        | |          
+                    | |     ___   __| | __| | ___  __| |          
+                    | |    / _ \ / _` |/ _` |/ _ \/ _` |          
+                    | |___| (_) | (_| | (_| |  __/ (_| |          
+                    |______\___/ \__,_|\__,_|\___|\__,_|          
+                                                                    
+                ]])
+end
 
 -- ตรวจสอบว่า request() ใช้ได้หรือไม่
 local requestFunction = (http_request or request or syn.request)
@@ -29,12 +47,12 @@ if response and response.Success then
     if data.success then
         if not data.isBanned then
             if data.hwid and data.hwid == tostring(HWID) then
-                print("Your Have Access")
+                FeariseLodded()
                 getgenv().path = data.gameNames
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/Malemz1/FORTUNE-HUB/refs/heads/main/CheckMapSrc.lua", true))()
             elseif data.hwid and data.hwid == "N/A" then
                 local postData = game.HttpService:JSONEncode({
-                    userKey = getgenv().Key,
+                    userKey = getgenv().key,
                     myHwid = HWID
                 })
                 
@@ -48,6 +66,7 @@ if response and response.Success then
                 })
 
                 if postResponse and postResponse.Success then
+                    FeariseLodded()
                     getgenv().path = data.gameNames
                     loadstring(game:HttpGet("https://raw.githubusercontent.com/Malemz1/FORTUNE-HUB/refs/heads/main/CheckMapSrc.lua", true))()
                 end
