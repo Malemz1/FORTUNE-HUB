416a4e27266950c7679a65c8a4d1d2de574594b1
Malemz1 <jaezabang@gmail.com>
2025-02-16 16:39:24 +0700

    Update CheckMapSrc.lua

--- CheckMapSrc.lua
+++ CheckMapSrc.lua
@@ -5,20 +5,27 @@ warn("Script Lodded")
             print("Bluelock : Rival Lodded")
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Malemz1/FORTUNE-HUB/refs/heads/main/BlueLock.lua", true))()
         else
-            player:Kick("Your Not Have Game Access")
+            game.Players.LocalPlayer:Kick("Your Not Have Game Access")
         end
     elseif game.PlaceId == 73956553001240 then
         if string.find(path, "VolleyballLegend") then
             print("Volleyball Legend Lodded")
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Malemz1/FORTUNE-HUB/refs/heads/main/Haikyuu.lua", true))()
         else
-            player:Kick("Your Not Have Game Access")
+            game.Players.LocalPlayer:Kick("Your Not Have Game Access")
         end
     elseif  game.PlaceId == 115815718131154 then
         if string.find(path, "KuroKuNoBasketball") then
             print("KuroKu No Basketball Lodded")
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Malemz1/FORTUNE-HUB/refs/heads/main/Kuroku.lua", true))()
         else
-            player:Kick("Your Not Have Game Access")
+            game.Players.LocalPlayer:Kick("Your Not Have Game Access")
+        end
+    elseif  game.PlaceId == 9301186334 then
+        if string.find(path, "RiderWorld") then
+            print("Rider : World Lodded")
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Malemz1/FORTUNE-HUB/refs/heads/main/Rider%20World.lua", true))()
+        else
+            game.Players.LocalPlayer:Kick("Your Not Have Game Access")
         end
     end
